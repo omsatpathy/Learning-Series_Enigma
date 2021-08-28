7a1e2860c63aadcbc047c2d24d3fd3760b0ff677
Om Satpathy <78610886+omsatpathy@users.noreply.github.com>
2021-08-28 22:55:43 +0530

    chopsticks code updated.

--- Python_tasks/Task1/Chopsticks_game.py
+++ Python_tasks/Task1/Chopsticks_game.py
@@ -1,6 +1,6 @@
 p1_leftCnt=1; p1_rightCnt=1; p2_leftCnt=1; p2_rightCnt=1;
 def error():
-    print("\nWrong Input.Please enter accordingly.");
+    print("\nWrong Input.Please enter accordingly.\n");
 def player_status():
     if p1_leftCnt==0 and p1_rightCnt==0:
         print("\nPlayer 2 won!!");
@@ -22,53 +22,79 @@ def check2():
         p2_rightCnt=0;
 def attack1():
     global p1_leftCnt,p2_leftCnt,p1_rightCnt,p2_rightCnt;
-    atk_move0,atk_move1=input("Enter attack combination : ").split(); atk_move0=atk_move0.upper(); atk_move1=atk_move1.upper();
-    if atk_move0=="L" and atk_move1=="L":
-        p2_leftCnt= p2_leftCnt + p1_leftCnt; 
-    elif atk_move0=='L' and atk_move1=='R':
-        p2_rightCnt=p2_rightCnt+ p1_leftCnt; 
-    elif atk_move0=='R' and atk_move1=='R':
-        p2_rightCnt=p2_rightCnt+p1_rightCnt; 
-    elif atk_move0=='R' and atk_move1=='L':
-        p2_leftCnt=p2_leftCnt+p1_rightCnt; 
-    else:
-        error(); attack1();
-    
+    try:
+        atk_move0,atk_move1=input("Enter attack combination : ").split(); atk_move0=atk_move0.upper(); atk_move1=atk_move1.upper(); 
+        if atk_move0=="L" and atk_move1=="L":
+            p2_leftCnt= p2_leftCnt + p1_leftCnt; 
+        elif atk_move0=='L' and atk_move1=='R':
+            p2_rightCnt=p2_rightCnt+ p1_leftCnt; 
+        elif atk_move0=='R' and atk_move1=='R':
+            p2_rightCnt=p2_rightCnt+p1_rightCnt; 
+        elif atk_move0=='R' and atk_move1=='L':
+            p2_leftCnt=p2_leftCnt+p1_rightCnt; 
+        else:
+            error(); attack1();
+    except ValueError:
+        print("Value Error detected.\n"); attack1();
 def attack2():
     global p1_leftCnt,p2_leftCnt,p1_rightCnt,p2_rightCnt;
-    atk_move0,atk_move1=input("Enter attack combination : ").split(); atk_move1=atk_move1.upper(); atk_move0=atk_move0.upper();
-    if atk_move0=='L' and atk_move1=='L':
-        p1_leftCnt= p1_leftCnt + p2_leftCnt; 
-    elif atk_move0=='L' and atk_move1=='R':
-        p1_rightCnt=p1_rightCnt+ p2_leftCnt; 
-    elif atk_move0=='R' and atk_move1=='R':
-        p1_rightCnt=p1_rightCnt+p2_rightCnt; 
-    elif atk_move0=='R' and atk_move1=='L':
-        p1_leftCnt=p1_leftCnt+p2_rightCnt; 
-    else:
-        error(); attack2();
+    try:
+        atk_move0,atk_move1=input("Enter attack combination : ").split(); atk_move1=atk_move1.upper(); atk_move0=atk_move0.upper();
+        if atk_move0=='L' and atk_move1=='L':
+            p1_leftCnt= p1_leftCnt + p2_leftCnt; 
+        elif atk_move0=='L' and atk_move1=='R':
+            p1_rightCnt=p1_rightCnt+ p2_leftCnt; 
+        elif atk_move0=='R' and atk_move1=='R':
+            p1_rightCnt=p1_rightCnt+p2_rightCnt; 
+        elif atk_move0=='R' and atk_move1=='L':
+            p1_leftCnt=p1_leftCnt+p2_rightCnt; 
+        else:
+            error(); attack2();
+    except ValueError:
+        print("Value Error detected.\n"); attack2();
 
 def split1():
     global p1_leftCnt,p2_leftCnt,p1_rightCnt,p2_rightCnt;
-    split_hand,split_left,split_right=input("Enter the move for split : ").split(); split_left=int(split_left); split_right=int(split_right);
-    if (p1_rightCnt+p1_leftCnt)==(split_left+split_right) and (split_left+split_right)<=8:
-        if split_right==5 or split_left==5 or split_left==0 or split_right==0:
-            print("You,sir,have walked towards your own descent.\n");
-        p1_leftCnt=split_left; p1_rightCnt=split_right;
-    else:
-        error(); split1();
+    try:
+        split_hand,split_left,split_right=input("Enter the move for split : ").split(); split_left=int(split_left); split_right=int(split_right);
+        if (p1_rightCnt+p1_leftCnt)==(split_left+split_right) and (split_left+split_right)<=8:
+            if split_right==5 or split_left==5 or split_left==0 or split_right==0:
+                print("You,sir,have walked towards your own descent.\n");
+            p1_leftCnt=split_left; p1_rightCnt=split_right;
+        else:
+            error(); split1();
+    except ValueError:
+        print("Value Error detected.\n"); split1();
 def split2():
     global p1_leftCnt,p2_leftCnt,p1_rightCnt,p2_rightCnt;
-    split_hand,split_left,split_right=input("Enter the move for split : ").split(); split_left=int(split_left); split_right=int(split_right);
-    if (p2_leftCnt+p2_rightCnt)==(split_left+split_right) and (split_left+split_right)<=8:
-        if split_right==5 or split_left==5 or split_left==0 or split_right==0:
-            print("You,sir,have walked towards your own descent.\n");
-        p2_leftCnt=split_left; p2_rightCnt=split_right; 
-    else:
-        error(); split2();
+    try:
+        split_hand,split_left,split_right=input("Enter the move for split : ").split(); split_left=int(split_left); split_right=int(split_right);
+        if (p2_leftCnt+p2_rightCnt)==(split_left+split_right) and (split_left+split_right)<=8:
+            if split_right==5 or split_left==5 or split_left==0 or split_right==0:
+                print("You,sir,have walked towards your own descent.\n");
+            p2_leftCnt=split_left; p2_rightCnt=split_right; 
+        else:
+            error(); split2();
+    except ValueError:
+        print("Value Error detected.\n"); split2();
 def display_status():
     print("\nCurrent status : \nPlayer 1 - ",p1_leftCnt,p1_rightCnt,"\nPlayer 2 - ",p2_leftCnt,p2_rightCnt,"\n");
-
+def player1():
+    p1=input("\nEnter move for player 1 : ");
+    if p1=='A' or p1=='a':
+        attack1();
+    elif p1=='S' or p1=='s':
+        split1();
+    else:
+        error(); player1();
+def player2():
+    p2=input("\nEnter move for player 2 : ");
+    if p2=='A' or p2=='a':
+        attack2();
+    elif p2=='S' or p2=='s':
+        split2();
+    else:
+        error(); player2(); 
 print("\t\t\t\tWELCOME TO THE CHOPSTICKS GAME.\n")
 print("MOVES ARE OF TWO TYPES: 'A' FOR ATTACK AND 'S' FOR SPLIT.\n")
 print("IN ATTACK COMBINATION USER INPUTS HAND TO ATTACK FROM AND HAND TO ATTACK TO.")
@@ -77,24 +103,12 @@ print("ONCE A PLAYER REACHES 5 FINGERS OR 0 FINGERS ON BOTH HANDS,OTHER PLAYER W
 print("IN PLAYER CURRENT STATUS,FIRST NUMBER INDICATES THE NUMBER OF FINGERS IN LEFT AND SECOND INDICATES NUMBER OF FINGERS IN RIGHT HAND\n")
 display_status();
 while(True): 
-    p1=input("\nEnter move for player 1 : ");
-    if p1=='A' or p1=='a':
-        attack1();
-    elif p1=='S' or p1=='s':
-        split1();
-    else:
-        error(); 
+    player1();
     check1();check2();
     display_status();
     if player_status()==0:
         break;
-    p2=input("\nEnter move for player 2 : ");
-    if p2=='A' or p2=='a':
-        attack2();
-    elif p2=='S' or p2=='s':
-        split2();
-    else:
-        error(); 
+    player2();
     check2();check1();
     display_status();
     if player_status()==0:
